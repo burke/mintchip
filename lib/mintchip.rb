40954ba6221b7ee5b31db8b40953356551ce9df1
Burke Libbey <burke@burkelibbey.org>
2012-04-11 16:13:34 -0500

    Switch to plain old Net::HTTP to have better access to error codes and such, and allow for future optimization

--- lib/mintchip.rb
+++ lib/mintchip.rb
@@ -1,17 +1,21 @@
 require 'openssl'
-require 'rest-client'
+require 'net/https'
+require 'uri'
 require 'json'
 require 'active_support/all'
 
 require "mintchip/version"
 
 module Mintchip
-
-  CURRENCY_CODES = {
-    CAD: 1
-  }
+  CURRENCY_CODES = { CAD: 1 }
 
   class InvalidCurrency < StandardError; end
+  class Error < StandardError ; end
+  class SystemError < Error   ; end
+  class CryptoError < Error   ; end
+  class FormatError < Error   ; end
+  class MintchipError < Error ; end
+
 
   def self.currency_code(name)
     CURRENCY_CODES[name.to_sym] or raise InvalidCurrency
@@ -25,7 +29,7 @@ module Mintchip
     end
 
     %w(id currencyCode balance creditLogCount debitLogCount creditLogCountRemaining debitLogCountRemaining maxCreditAllowed maxDebitAllowed version).each do |attr|
-      define_method attr.underscore { info[attr] }
+      define_method(attr.underscore) { info[attr] }
     end
 
     # GET /info/{responseformat}
@@ -35,14 +39,14 @@ module Mintchip
 
     # POST /receipts
     def load_value(vm)
-      res = post "/receipts", vm, {"Content-Type" => "application/vnd.scg.ecn-message"}
-
+      res = post "/receipts", vm, "application/vnd.scg.ecn-message"
+      # body is an empty string on success, which is gross.
       res == ""
     end
 
     # POST /payments/request
     def create_value1(vrm)
-      post "/payments/request", {"Value Request Message" => vrm}, {"Content-Type" => "application/vnd.scg.ecn-request"}
+      post "/payments/request", vrm, "application/vnd.scg.ecn-request"
     end
 
     def create_value2(payee_id, currency_name, amount_in_cents)
@@ -67,16 +71,46 @@ module Mintchip
 
     private
 
-    def get(path)
-      resource = RestClient::Resource.new(RESOURCE_BASE + path, ssl_client_key: @p12.key, ssl_client_cert: @p12.certificate)
-      resource.get
+    def connection
+      uri               = URI.parse RESOURCE_BASE
+      https             = Net::HTTP.new(uri.host, uri.port)
+      https.use_ssl     = true
+      https.cert        = @p12.certificate
+      https.key         = @p12.key
+      https.verify_mode = OpenSSL::SSL::VERIFY_NONE # TODO: Fix this.
+      # https.ca_file   = File.join(TestDataPath, 'cacert.pem')
+
+      https
     end
 
-    def post(path, data = {}, headers = {})
-      resource = RestClient::Resource.new(RESOURCE_BASE + path, ssl_client_key: @p12.key, ssl_client_cert: @p12.certificate)
-      resource.post(data, headers)
+    def get(path)
+      uri = URI.parse(RESOURCE_BASE + path)
+      req = Net::HTTP::Get.new(uri.path)
+      handle_response connection.start { |cx| cx.request(req) }
+    end
+
+    def post(path, data = {}, content_type = nil)
+      uri = URI.parse(RESOURCE_BASE + path)
+      req = Net::HTTP::Post.new(uri.path)
+
+      Hash === data ? req.set_form_data(data) : req.body = data
+      req.content_type = content_type if content_type
+
+      handle_response connection.start { |cx| cx.request(req) }
+    end
+
+    def handle_response(resp)
+      case resp.code.to_i
+      when 200 ; resp.body
+      when 452 ; raise Mintchip::SystemError, resp.msg
+      when 453 ; raise Mintchip::CryptoError, resp.msg
+      when 454 ; raise Mintchip::FormatError, resp.msg
+      when 455 ; raise Mintchip::MintchipError, resp.msg
+      else     ; raise Mintchip::Error, resp.msg
+      end
     end
 
   end
 
 end
+
